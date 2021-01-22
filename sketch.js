1f5835b3bef95fd606ced005f27009fda6e64666
Camren10 <72534907+Camren10@users.noreply.github.com>
2021-01-21 19:59:05 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,9 +10,10 @@ var invisibleGround;
 var sling;
 var gamestate="onSling";
 var backboard,rim1,rim2,rim3,rim4Sprite;
-
+var score=0;
 function preload() {
   backgroundImg=loadImage("images/background.jpg");
+  hoopImage=loadImage("images/hoop.png");
 }
 
 function setup() {
@@ -20,30 +21,38 @@ function setup() {
 
   engine=Engine.create();
   world=engine.world;
-
   basketball = new Ball(200,height/2);
 
   invisibleGround = new Ground(width/2,height-50,width,10);
-  backboard = new Ground(width-200,200,15,150);
-  rim1 = new Ground(width-250,360,15,150);
-  rim2 = new Ground(width-230,275,55,15);
-  rim3 = new Ground(width-400,360,15,150);
-
+  //backboard = new Hoop(width-800,200);
+  //rim1 = new Hoop(width-800,360,800,600);
+  //rim2 = new Hoop(width-230,275,55,15);
+  //rim3 = new Hoop(width-400,360,15,150);
+  A = createSprite(width-250,400,10,200);
   sling = new Sling(basketball.body,{x: 200,y: height/2+100});
 }
 
 function draw() {
-  background(backgroundImg);  
+  background(backgroundImg); 
+  textSize(18); 
+  text(mouseX+","+mouseY,mouseX,mouseY);
   Engine.update(engine);
-
+  image(hoopImage,width-800,200);
   basketball.display();
   sling.display();
 
-  backboard.display();
-  invisibleGround.display();
-  rim1.display();
-  rim2.display();
-  rim3.display();
+  text("points: "+score,width-150,100);
+
+  //backboard.display();
+  //invisibleGround.display();
+  //rim1.display();
+  //rim2.display();
+  //rim3.display();
+
+  if(basketball.body.position.x>1650 && basketball.body.position.x<1750 && 
+    basketball.body.position.y>400 && basketball.body.position.y<470) {
+    score=score+1;
+  }
 
   drawSprites();
 }
